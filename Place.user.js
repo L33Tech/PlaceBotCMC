401ca2d5abf42ddead7510d0b7db4842eada9457
Leet <36166244+L33Tech@users.noreply.github.com>
2022-04-03 16:46:52 -0400

    Update Place.user.js

--- Place.user.js
+++ Place.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name         PlaceDE Bot
-// @namespace    https://github.com/PlaceDE/Bot
+// @name         SMP Online Place Bot
+// @namespace    https://github.com/L33Tech/PlaceBotCMC/
 // @version      16
 // @description  /r/place bot
 // @author       NoahvdAa, reckter, SgtChrome, nama17
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @require	     https://cdn.jsdelivr.net/npm/toastify-js
+// @require	 https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/PlaceDE/Bot/raw/main/placedebot.user.js
-// @downloadURL  https://github.com/PlaceDE/Bot/raw/main/placedebot.user.js
+// @updateURL    https://github.com/L33Tech/PlaceBotCMC/raw/main/Place.user.js
+// @downloadURL  https://github.com/L33Tech/PlaceBotCMC/raw/main/Place.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
